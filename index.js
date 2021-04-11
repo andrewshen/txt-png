2bbaf555d1d33ec9f3c0e461ec9cb61cc5ed1c98
Andrew Shen <andrewshen@gmail.com>
2021-04-10 22:29:37 -0700

    Strip formatting from contenteditable

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import domtoimage from 'dom-to-image';
 import 'regenerator-runtime/runtime';
 
 const txt = document.getElementById('txt');
-const img = document.getElementById('img');
 
 const setToClipboard = async (blob) => {
   const data = [new ClipboardItem({ [blob.type]: blob })];
@@ -20,6 +19,21 @@ const generateImage = () => {
     });
 };
 
-document
-  .getElementById('txt')
-  .addEventListener('input', (event) => generateImage());
+txt.addEventListener('input', (e) => generateImage());
+txt.addEventListener('paste', (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  let text = '';
+  const event = e.originalEvent || e;
+  if (event.clipboardData && event.clipboardData.getData) {
+    text = event.clipboardData.getData('text/plain').trim();
+  } else if (window.clipboardData && window.clipboardData.getData) {
+    text = window.clipboardData.getData('Text').trim();
+  }
+  if (document.queryCommandSupported('insertText')) {
+    document.execCommand('insertText', false, text);
+  } else {
+    document.execCommand('paste', false, text);
+  }
+});
+txt.focus();
